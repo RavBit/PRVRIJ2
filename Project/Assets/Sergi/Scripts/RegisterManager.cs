8d734b1b49b9ca6e9b7e5a88f0164b966b4dc1cb
Sergi van Ravenswaay <sergi@live.nl>
2018-06-14 14:54:57 +0200

    Last fixes game

---
+++ Project/Assets/Sergi/Scripts/RegisterManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterManager : MonoBehaviour {
+    [Header("UI Objects")]
+    public Dropdown CompanyTypeDropDown;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
